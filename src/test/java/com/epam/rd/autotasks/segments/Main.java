531691a6dc478805fdd83437c8fe4e64c8f7bfde
Yauheni Rudzko <e-rudko2000@mail.ru>
2022-11-15 15:35:32 +0300

    Method length, method intersection and middle

--- src/test/java/com/epam/rd/autotasks/segments/Main.java
+++ src/test/java/com/epam/rd/autotasks/segments/Main.java
@@ -18,6 +18,14 @@ public class Main {
         }
 
         {
+            Segment segment = new Segment(new Point(2, 0), new Point(0, 2));
+            Point midpoint = segment.middle();
+
+            System.out.println(midpoint.getX());
+            System.out.println(midpoint.getY());
+        }
+
+        {
             Segment first = new Segment(new Point(0, 0), new Point(4, 0));
             Segment second = new Segment(new Point(2, 1), new Point(1, 2));
             Point intersection = first.intersection(second);
